bd682ff7218072054d9c2a18df3a4a19366d02c5
Arjan van Olphen <arjan@scanframe.nl>
2024-01-30 19:49:54 +0100

    Updated mion script.

--- minio/bin/login.sh
+++ minio/bin/login.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-mc config host add minio "http://localhost:9000" "${MINIO_ACCESS_KEY}" "${MINIO_SECRET_KEY}"
-
+mc config host add minio "${MINIO_URL}" "${MINIO_ACCESS_KEY}" "${MINIO_SECRET_KEY}"
