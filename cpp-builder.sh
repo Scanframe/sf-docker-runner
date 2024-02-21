1387992a5b62dc0ae3e00e8440b6e10f39e25f89
Arjan van Olphen <arjan@scanframe.nl>
2024-02-21 01:43:47 +0100

    Reverted entrypoint.sh

--- cpp-builder.sh
+++ cpp-builder.sh
@@ -25,10 +25,10 @@ function ShowHelp {
     -p, --project : Project directory which is mounted in '/mnt/project' and has a symlink '~/project'.
 
   Commands:
-    build     : Builds the docker image tagged 'gnu-cpp:dev' for self hosted Nexus repository and requires zipped Qt libraries.
-    push      : Pushes the docker image to the self hosted Nexus repository.
-    pull      : Pulls the docker image from the self hosted Nexus repository.
-    base-push : Pushes the base image '${BASE_IMG_NAME}' to the self hosted Nexus repository.
+    build     : Builds the docker image tagged 'gnu-cpp:dev' for self-hosted Nexus repository and requires zipped Qt libraries.
+    push      : Pushes the docker image to the self-hosted Nexus repository.
+    pull      : Pulls the docker image from the self-hosted Nexus repository.
+    base-push : Pushes the base image '${BASE_IMG_NAME}' to the self-hosted Nexus repository.
     info      : Show general docker information.
     prune     : Remove all Docker build cache.
     login     : Log Docker in on the Nexus repository.
@@ -262,7 +262,7 @@ case "${cmd}" in
 
 	versions)
 		# Just reenter the script using the the correct arguments.
-		"${0}" run -- /bin/bash -c "\${HOME}/bin/versions.sh"
+		"${0}" run -- /bin/bash -c versions.sh
 		;;
 
 	run)
